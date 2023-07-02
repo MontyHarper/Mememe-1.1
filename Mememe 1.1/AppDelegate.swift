877bb29ab8308383833cecde397db0c925d56496
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-07-01 23:12:40 -0500

    Resolved Conflicts
    Resolved conflicts from merge.

--- Mememe 1.1/AppDelegate.swift
+++ Mememe 1.1/AppDelegate.swift
@@ -10,11 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-<<<<<<< Updated upstream
 
-
-=======
->>>>>>> Stashed changes
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
